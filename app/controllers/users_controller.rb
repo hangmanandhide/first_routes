a78a0ceb207916448739cb775942d05c883807dd
hangmanandhide <9014584+hangmanandhide@users.noreply.github.com>
2019-09-05 23:30:47 -0700

    Added Users model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
-class UsersController < ApplicationController < ActionController::Base
+class UsersController < ApplicationController
+  
+  def index
+    render json: User.all  
+  end
+
+  def create
+    render json: params
+  end
+
+  def show
+    render json: params
+  end
+
 end
