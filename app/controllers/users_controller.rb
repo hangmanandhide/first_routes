cb891f84928be4abec630ec7fcacd4a355a543b2
hangmanandhide <9014584+hangmanandhide@users.noreply.github.com>
2019-09-06 00:44:42 -0700

    Added User#Create Show Update and Destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,55 @@
 class UsersController < ApplicationController
   
   def index
-    render json: User.all  
+    if User.count == 0
+      render plain: "There are no users!"
+    else 
+      render json: User.all  
+    end
   end
 
   def create
-    render json: params
+    user = User.new(user_param)
+
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+
   end
 
   def show
-    render json: params
+    found = User.find(params[:id])
+    if found
+      render json: found
+    else
+      render json: found.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update_attributes(user_param)
+      redirect_to :action => 'show', :id => user
+    else
+      user = User.all
+      render :action => 'edit'
+    end
+
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to :action => 'index'
+  end
+
+
+  private
+
+  def user_param
+    params.require(:user).permit(:name, :email)
   end
 
 end
