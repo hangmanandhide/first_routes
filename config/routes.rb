c8ff30a15f08b67a2ea511cf7ab6ae42c579c0a2
hangmanandhide <9014584+hangmanandhide@users.noreply.github.com>
2019-09-05 18:24:51 -0700

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :users
+
+  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+  patch 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy'
+  put 'users/:id', to: 'users#update'
+  post 'users/:id', to: 'users#create'
+  get 'users', to: 'users#index', as: 'users'
+  get 'users/new', to: 'users#new', as: 'new_user'
+
 end
