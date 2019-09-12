04b6aeaeb19e625269cb15621895a40e38d6d5e9
hangmanandhide <9014584+hangmanandhide@users.noreply.github.com>
2019-09-12 14:43:34 -0700

    Setup user-test, though failing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+
+  before_save { username.downcase! }
   validates :username, presence: true, uniqueness: true
+
+  has_many :artworks,
+    foreign_key: :artist_id
+  
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
+require "minitest/reporters"
+Minitest::Reporters.use!
+require 'byebug'
 
 class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+
+  def setup
+    @user = User.new(username: "Foobar")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "username should be present" do
+    @user.username = "        "
+    assert_not @user.valid?
+  end
+
+  test "username should be unique" do
+    original_user = @user.dup
+    original_user.save
+    duplicate_user = @user.dup
+    duplicate_user.username = original_user.username.upcase
+    # duplicate_user.save
+
+    assert_not duplicate_user.valid?
+
+    # duplicate_user = User.new(username: "Foobar")
+    # duplicate_user.save
+    # assert_not duplicate_user.valid?
+  end
+
 end
